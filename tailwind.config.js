c7e37ac127fe132c759e2996f52b559340e564ec
shota <morgen.ist.auch.noch.ein.tag@gmail.com>
2021-05-11 21:55:49 +0900

    ✨ ユーザアイコンhover時メニュー表示追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   purge: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
@@ -7,5 +9,18 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/aspect-ratio")],
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".hover-trigger .hover-target": {
+          display: "none",
+        },
+        ".hover-trigger:hover .hover-target": {
+          display: "block",
+        },
+      };
+      addUtilities(newUtilities);
+    },
+  ],
 };
