d65eff16d822d941f7be268bdbbf2a386c946c47
shota <morgen.ist.auch.noch.ein.tag@gmail.com>
2021-06-02 11:27:29 +0900

    🚧 amplify add api

---
+++ src/graphql/queries.ts
@@ -0,0 +1,37 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getArticle = /* GraphQL */ `
+  query GetArticle($id: ID!) {
+    getArticle(id: $id) {
+      id
+      uid
+      author
+      caption
+      imageUrl
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listArticles = /* GraphQL */ `
+  query ListArticles(
+    $filter: ModelArticleFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listArticles(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        uid
+        author
+        caption
+        imageUrl
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
---
+++ src/graphql/subscriptions.ts
@@ -0,0 +1,43 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateArticle = /* GraphQL */ `
+  subscription OnCreateArticle {
+    onCreateArticle {
+      id
+      uid
+      author
+      caption
+      imageUrl
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateArticle = /* GraphQL */ `
+  subscription OnUpdateArticle {
+    onUpdateArticle {
+      id
+      uid
+      author
+      caption
+      imageUrl
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteArticle = /* GraphQL */ `
+  subscription OnDeleteArticle {
+    onDeleteArticle {
+      id
+      uid
+      author
+      caption
+      imageUrl
+      createdAt
+      updatedAt
+    }
+  }
+`;
